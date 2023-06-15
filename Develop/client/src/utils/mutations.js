adb188b6fb6e24d71fa809c92e48845d97e04c96
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-15 15:26:53 -0300

    finally able to remove books. The problem was with my mutation

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -57,12 +57,32 @@ export const SAVE_BOOK = gql`
 `;
 
 
+// export const REMOVE_BOOK = gql`
+//     mutation removeBook($bookId: String!) {
+//         removeBook(bookId: $bookId) {
+//             _id
+//             username
+//             savedBooks
+//         }
+//     }
+// `;
+
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: String!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            savedBooks
-        }
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
+  }
 `;
+
